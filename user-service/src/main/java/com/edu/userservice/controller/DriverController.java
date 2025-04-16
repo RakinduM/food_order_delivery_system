2fc8014cf930a1dd0c1f5c29a15d586c9099e181
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-04-17 02:27:43 +0530

    add gcp bucket for storing image

--- user-service/src/main/java/com/edu/userservice/controller/DriverController.java
+++ user-service/src/main/java/com/edu/userservice/controller/DriverController.java
@@ -5,24 +5,52 @@ import com.edu.userservice.dto.AuthResponse;
 import com.edu.userservice.dto.CustomerRegRequest;
 import com.edu.userservice.dto.DriverRegRequest;
 import com.edu.userservice.service.DriverService;
+import com.edu.userservice.service.GcsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/driver")
 @RequiredArgsConstructor
 public class DriverController {
     private final DriverService driverService;
+    private final GcsService gcsService;
 
-    @PostMapping("/register")
+    @PostMapping(value = "/register", consumes = "multipart/form-data")
     public ResponseEntity<AuthResponse> register(
-            @Valid @RequestBody DriverRegRequest request
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            @RequestParam("email") String email,
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName") String lastName,
+            @RequestParam("number") String number,
+            @RequestParam("nicImage") MultipartFile nicImage
     ) {
-        return new ResponseEntity<>(driverService.register(request), HttpStatus.CREATED);
+        try {
+            String nicImageUrl = gcsService.uploadFile(nicImage);
+
+            DriverRegRequest request = DriverRegRequest.builder()
+                    .firstName(firstName)
+                    .lastName(lastName)
+                    .username(username)
+                    .nic(nicImageUrl)
+                    .email(email)
+                    .phoneNumber(number)
+                    .password(password)
+                    .build();
+
+            AuthResponse response = driverService.register(request);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(AuthResponse.builder().message("Error: " + e.getMessage()).build());
+        }
     }
 
     @PostMapping("/login")
