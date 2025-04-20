c745143c786e14529e68a6d68533abd1b07d9ce6
amaya2509 <amayafernando150@gmail.com>
2025-04-20 17:39:15 +0530

    refactor: remove Autowired

--- order-service/src/main/java/com/edu/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/edu/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.edu.orderservice.dto.OrderRequestDTO;
 import com.edu.orderservice.dto.OrderResponseDTO;
 import com.edu.orderservice.model.OrderStatus;
 import com.edu.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/orders")
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
+    private final OrderService orderService;
 
     @PostMapping
     public ResponseEntity<OrderResponseDTO> placeOrder(@RequestBody OrderRequestDTO dto){
