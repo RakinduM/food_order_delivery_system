a3d140015d7b9d0fd9231c40a16b37c964445faf
amaya2509 <amayafernando150@gmail.com>
2025-04-29 01:22:48 +0530

    fixed payment controller

--- payment-service/src/main/java/com/edu/paymentservice/controller/ProductCheckoutController.java
+++ payment-service/src/main/java/com/edu/paymentservice/controller/ProductCheckoutController.java
@@ -3,6 +3,7 @@ package com.edu.paymentservice.controller;
 import com.edu.paymentservice.dto.ProductRequest;
 import com.edu.paymentservice.dto.StripeResponse;
 import com.edu.paymentservice.service.StripeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/product/v1")
 public class ProductCheckoutController {
 
-    private StripeService stripeService;
-
-    public ProductCheckoutController(StripeService stripeService) {
-        this.stripeService = stripeService;
-    }
+    private final StripeService stripeService;
 
     @PostMapping("/checkout")
     public ResponseEntity<StripeResponse> checkoutProducts (@RequestBody ProductRequest productRequest) {
